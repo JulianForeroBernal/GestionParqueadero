bbc52063ad441baa546b7e64e44a83b32036bbd3
camily-l <mcamilaandrea229@gmail.com>
2026-05-15 22:24:35 -0500

    Conexion a base de datos corregida

--- database/database_parqueadero.sql
+++ database/database_parqueadero.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `Parqueadero` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `Parqueadero`;
+CREATE DATABASE  IF NOT EXISTS `parqueadero` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `parqueadero`;
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: Parqueadero
+-- Host: 127.0.0.1    Database: parqueadero
 -- ------------------------------------------------------
--- Server version	9.6.0
+-- Server version	8.4.9
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,15 +16,118 @@ USE `Parqueadero`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
-SET @@SESSION.SQL_LOG_BIN= 0;
 
 --
--- GTID state at the beginning of the backup 
+-- Table structure for table `parqueaderos`
 --
 
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'a01472a2-4da7-11f1-b49f-7245a5f8cd4e:1-25';
-SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+DROP TABLE IF EXISTS `parqueaderos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `parqueaderos` (
+                                `id` int NOT NULL AUTO_INCREMENT,
+                                `zona` enum('CARRO','MOTO','BICICLETA') NOT NULL,
+                                `capacidad` int NOT NULL,
+                                `ocupados` int DEFAULT '0',
+                                PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `parqueaderos`
+--
+
+LOCK TABLES `parqueaderos` WRITE;
+/*!40000 ALTER TABLE `parqueaderos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `parqueaderos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registros`
+--
+
+DROP TABLE IF EXISTS `registros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `registros` (
+                             `id` int NOT NULL AUTO_INCREMENT,
+                             `hora_entrada` datetime NOT NULL,
+                             `hora_salida` datetime DEFAULT NULL,
+                             `vehiculo_id` int NOT NULL,
+                             `parqueadero_id` int NOT NULL,
+                             PRIMARY KEY (`id`),
+                             KEY `vehiculo_id` (`vehiculo_id`),
+                             KEY `parqueadero_id` (`parqueadero_id`),
+                             CONSTRAINT `registros_ibfk_1` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculos` (`id`),
+                             CONSTRAINT `registros_ibfk_2` FOREIGN KEY (`parqueadero_id`) REFERENCES `parqueaderos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registros`
+--
+
+LOCK TABLES `registros` WRITE;
+/*!40000 ALTER TABLE `registros` DISABLE KEYS */;
+/*!40000 ALTER TABLE `registros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+                            `id` int NOT NULL AUTO_INCREMENT,
+                            `nombre` varchar(100) NOT NULL,
+                            `dni` varchar(20) NOT NULL,
+                            `correo` varchar(100) NOT NULL,
+                            PRIMARY KEY (`id`),
+                            UNIQUE KEY `dni` (`dni`),
+                            UNIQUE KEY `correo` (`correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehiculos`
+--
+
+DROP TABLE IF EXISTS `vehiculos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehiculos` (
+                             `id` int NOT NULL AUTO_INCREMENT,
+                             `codigo_barras` varchar(100) NOT NULL,
+                             `tipo` enum('CARRO','MOTO','BICICLETA') NOT NULL,
+                             `placa` varchar(20) NOT NULL,
+                             `usuario_id` int NOT NULL,
+                             PRIMARY KEY (`id`),
+                             UNIQUE KEY `codigo_barras` (`codigo_barras`),
+                             UNIQUE KEY `placa` (`placa`),
+                             KEY `usuario_id` (`usuario_id`),
+                             CONSTRAINT `vehiculos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehiculos`
+--
+
+LOCK TABLES `vehiculos` WRITE;
+/*!40000 ALTER TABLE `vehiculos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehiculos` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -35,4 +138,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-13 17:36:12
+-- Dump completed on 2026-05-15 22:21:46
